e13a155678b87dbce2197dcce9643e2177f567f2
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-09-20 13:06:06 -0300

    functionality improvements, useEffect + resumen con states ok

--- app/api/calculoDevaluacion/route.ts
+++ app/api/calculoDevaluacion/route.ts
@@ -3,5 +3,5 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
     const { sueldo, fechaUltimoAumento } = await request.json();
     console.log( sueldo, fechaUltimoAumento );
-    return NextResponse.json({ sueldo, fechaUltimoAumento}, {status: 200});
+    return NextResponse.json({ sueldo: sueldo, fechaUltimoAumento: fechaUltimoAumento}, {status: 200});
 }
--- app/testing/form/page.tsx
+++ app/testing/form/page.tsx
@@ -22,8 +22,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { FormEvent, useState } from "react"
+import { useEffect, useState } from "react"
 import { CardContent } from "@/components/ui/card"
 import { ArrowLeftIcon, ArrowRightIcon, ArrowTopRightIcon, CalendarIcon } from "@radix-ui/react-icons"
 import { addDays, format } from "date-fns"
@@ -32,8 +31,8 @@ import { es } from "date-fns/locale"
 
 const formSchema = z.object({
   sueldo: z.coerce.number({
-    required_error: "Ingresá tu sueldo",
-    invalid_type_error: "Ingresá tu sueldo",
+    required_error: "Ingresá un valor.",
+    invalid_type_error: "Ingresá un valor.",
   })
   .int({message: 'Ingresá un número entero.'})
   .positive({	message: 'El valor ingresado debe ser > 0' })
@@ -49,7 +48,10 @@ export default function ProfileForm() {
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    mode: "onChange",
   })
+
+  // console.log(form.watch("sueldo"))
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof formSchema>) {
@@ -58,16 +60,15 @@ export default function ProfileForm() {
         method: 'POST', 
         body: JSON.stringify(values)
       })
-    } catch (error) {
-      
+      const data = await res.json()
+    } catch (error: any) {
+        throw new Error(error.message)
     }
-    alert(JSON.stringify(values, null, 2))
-    console.log(JSON.stringify(values))
+
   }
 
 
   // Calendar functionality
-  const [date, setDate] = useState<Date | undefined>()
   const disabledDays = [
     { from: new Date(0, 1, 1), to: new Date(2021, 11, 31) }, 
     { from: addDays(new Date(), 1), to: new Date(3000, 11, 31) }
@@ -75,10 +76,21 @@ export default function ProfileForm() {
 
   // Multi-step form functionality
   const [formStep, setFormStep] = useState(0)
+  console.log(formStep)
+
+  // states
+  const [sueldoIngresado, setSueldoIngresado] = useState(0)
+  const [fechaIngresada, setFechaIngresada] = useState(new Date())
+
+  useEffect(() => {
+    console.log("Sueldo ingresado:", sueldoIngresado);
+    console.log("Fecha ingresado:", fechaIngresada);  
+  }, [sueldoIngresado, fechaIngresada])
 
   return (
     <CardContent className="flex justify-center items-center">
 
+      {/* Botón Comenzar */}
       {formStep === 0 && 
         <Button onClick={() => {setFormStep(1)}}
             className="flex justify-between items-center gap-2 text-xl font-bold p-6 hover:border border-primary/50 ease-in-out duration-300" 
@@ -88,9 +100,11 @@ export default function ProfileForm() {
         </Button>
       }
 
+      {/* Formulario */}
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          
+        <form onSubmit={form.handleSubmit(onSubmit)} noValidate className="space-y-8">
+
+          {/* Input Sueldo */}
           <div className={cn({hidden: formStep !== 1})}>
             <FormField
               control={form.control}
@@ -99,24 +113,22 @@ export default function ProfileForm() {
                 <FormItem>
                   <FormLabel>Ingresá tu sueldo en pesos.</FormLabel>
                   <FormControl>
-                    <Input type="number" {...field} className="bg-primary/20 border border-primary/60 p-6 text-2xl text-center font-bold rounded-md"/>
+                    <Input type="number" {...field} value={field.value || ''} className="bg-primary/20 border border-primary/60 p-6 text-2xl text-center font-bold rounded-md"/>
                   </FormControl>
-                  <FormDescription>
                   <FormMessage />
-                  </FormDescription>
-                  
                 </FormItem>
               )}
             />
           </div>
           
+          {/* Input Fecha Ultimo Aumento */}
           <div className={cn({hidden: formStep !== 2})}>
             <FormField
             control={form.control}
             name="fechaUltimoAumento"
             render={({ field }) => (
               <FormItem className="flex flex-col">
-                <FormLabel className="text-xs sm:text-sm">¿Cuándo te aumentaron por última vez?</FormLabel>
+                <FormLabel className="text-xs sm:text-sm">Desde cuándo cobrás ese monto?</FormLabel>
                 <Popover>
                   <PopoverTrigger asChild>
                     <FormControl>
@@ -145,15 +157,26 @@ export default function ProfileForm() {
                   />
                   </PopoverContent>
                 </Popover>
-                <FormDescription>
-                  <FormMessage />
-                </FormDescription>
+                <FormMessage />
               </FormItem>
             )}
             />
           </div>
+          
+          {/* Resumen */}
+          <div className={cn('flex flex-col justify-between gap-2',{hidden: formStep !== 3})}>
+              <p className="text-2xl font-bold">Confirmá tus datos</p>
+              <span>Sueldo:</span>
+              <p className="text-2xl sm:text-3xl font-extrabold text-green-600">
+                {`$ ${new Intl.NumberFormat('es-sp', { style: 'currency', currency: 'ARS' }).format(sueldoIngresado)}`}
+              </p>
+              <span className="">Cobrás ese monto desde:</span>
+              <p className="text-2xl sm:text-3xl font-extrabold text-green-600">{format(fechaIngresada, "PPP", { locale: es })}</p>
+          </div>
 
+          {/* Botones Volver/Siguiente/Submit */}
           <div className="flex justify-between items-center">
+                {/* Botón Volver */}
                 <Button
                   type="button"
                   variant={"ghost"}
@@ -167,28 +190,45 @@ export default function ProfileForm() {
                   <ArrowLeftIcon className="w-4 h-4 mr-2" />
                   Volver
                 </Button>
+                
+                {/* Botón Siguiente */}
                 <Button
                   type="button"
                   variant={"ghost"}
                   className={cn({
-                    hidden: formStep !== 1 ,
+                    hidden: formStep == 0 || formStep == 3 ,
                   })}
                   onClick={() => {
                     // validation
-                    form.trigger(["sueldo"]);
-                    const sueldoState = form.getFieldState("sueldo");
-                    if (!sueldoState.isDirty || sueldoState.invalid) return;
+                    if (formStep == 1) {
+                      const sueldoInputState = form.getFieldState("sueldo");
+                      if (!sueldoInputState.isDirty || sueldoInputState.invalid) {
+                        return;
+                        } else {
+                          setSueldoIngresado(Number(form.getValues("sueldo")))
+                          setFormStep(2);
+                          }
+                    } else if (formStep == 2) { 
+                      const fechaInputState = form.getFieldState("fechaUltimoAumento");
+                      if (!fechaInputState.isDirty || fechaInputState.invalid) {
+                        return;
+                        } else {
+                          setFechaIngresada(form.getValues("fechaUltimoAumento"))
+                          setFormStep(3);
+                          }
+                    }
 
-                    setFormStep(2);
-                  }}
+                    }}
                 >
                   Siguiente
                   <ArrowRightIcon className="w-4 h-4 ml-2" />
                 </Button>
+
+                {/* Botón Submit */}
                 <Button
                   type="submit"
                   className={cn({
-                    hidden: formStep !== 2,
+                    hidden: formStep !== 3,
                   })}
                 >
                   Calculá
